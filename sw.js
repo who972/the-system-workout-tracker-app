1901fab67bf1ca8a2eca3f8e19529b3509168d05
who972 <torres.edward@ymail.com>
2026-09-23 18:39:46 -0500

    Refresh PWA cache for V2 screen navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "the-system-workout-tracker-rc1";
+const CACHE = "the-system-workout-tracker-v2-screen-nav-1";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -8,9 +8,9 @@ self.addEventListener("activate", event => {
 });
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+  event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
-  }).catch(() => caches.match("./index.html"))));
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html"))));
 });
