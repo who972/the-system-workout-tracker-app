d81c14a598ec31e88818f892e26397ab0c9c967f
who972 <torres.edward@ymail.com>
2026-09-25 11:54:43 -0500

    Refresh cache for System OS shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "the-system-workout-tracker-v9-health-connect";
+const CACHE = "the-system-os-v14-shell";
 const ASSETS = ["./", "./index.html", "./styles.css", "./side-system.css", "./app.js", "./health-connect.js", "./side-system.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
