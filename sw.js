4fa635f2bd45d3c5dcf5ed709a2c824c41fff615
who972 <torres.edward@ymail.com>
2026-09-25 11:59:36 -0500

    Refresh cache for Mission HUD build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "the-system-os-v14-modules";
+const CACHE = "the-system-os-v15-mission-hud";
 const ASSETS = ["./", "./index.html", "./styles.css", "./side-system.css", "./app.js", "./health-connect.js", "./side-system.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
