7a512408690a5fe72d24fdd69add0cac7d9f72f7
who972 <torres.edward@ymail.com>
2026-09-26 01:03:38 -0500

    Rotate PWA cache for competitive results

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "the-system-os-v34-social-lifecycle";
+const CACHE = "the-system-os-v35-results";
 const ASSETS = ["./", "./index.html", "./styles.css", "./side-system.css", "./app.js", "./health-connect.js", "./side-system.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
