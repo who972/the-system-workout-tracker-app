18fc78afa5b06cd0680c0beab68dd24440ac376d
who972 <torres.edward@ymail.com>
2026-09-26 01:07:08 -0500

    Rotate PWA cache for Combat Record

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "the-system-os-v35-results";
+const CACHE = "the-system-os-v36-combat-record";
 const ASSETS = ["./", "./index.html", "./styles.css", "./side-system.css", "./app.js", "./health-connect.js", "./side-system.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
