96a9571bc9f58f5b4fd76f25cf2bfde67a3d4b55
who972 <torres.edward@ymail.com>
2026-09-26 13:46:30 -0500

    Package V40 true 3D Central Command orbit

--- android-wrapper/app/build.gradle.kts
+++ android-wrapper/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.thesystem.workouttracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 39
-        versionName = "0.39"
+        versionCode = 40
+        versionName = "0.40"
     }
 
     buildTypes {
