d25b127df47b97027af0ca129fcfadd0cd5027f9
who972 <torres.edward@ymail.com>
2026-09-26 01:41:33 -0500

    Align Android Java and Kotlin JVM targets

--- android-wrapper/app/build.gradle.kts
+++ android-wrapper/app/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-android {
-    namespace = "com.thesystem.workouttracker"
+android {\n    compileOptions {\n        sourceCompatibility = JavaVersion.VERSION_17\n        targetCompatibility = JavaVersion.VERSION_17\n    }\n    kotlinOptions {\n        jvmTarget = "17"\n    }\n    namespace = "com.thesystem.workouttracker"
     compileSdk = 36
     defaultConfig {
         applicationId = "com.thesystem.workouttracker"
